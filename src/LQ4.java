b53a9b45ced9850903449bb59d887193db8706f2
Apoorva Kharga <apoorvakharga@gmail.com>
2026-01-16 13:31:26 +0530

    Initial

---
+++ src/LQ4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+interface Employee {
+    void getDetails();
+}
+
+interface ManagerDept extends Employee {
+    void getDeptDetails();
+}
+
+class Head implements ManagerDept {
+    int empId;
+    String empName;
+    int deptId;
+    String deptName;
+
+    Scanner sc = new Scanner(System.in);
+
+    public void getDetails() {
+        System.out.print("Enter employee id: ");
+        empId = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter employee name: ");
+        empName = sc.nextLine();
+    }
+
+    public void getDeptDetails() {
+        System.out.print("Enter department id: ");
+        deptId = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter department name: ");
+        deptName = sc.nextLine();
+    }
+
+    void display() {
+        System.out.println("Employee id - " + empId);
+        System.out.println("Employee name - " + empName);
+        System.out.println("Department id - " + deptId);
+        System.out.println("Department name - " + deptName);
+    }
+}
+
+public class LQ4 {
+    public static void main(String[] args) {
+        Head h = new Head();
+        h.getDetails();
+        h.getDeptDetails();
+        h.display();
+    }
+}
